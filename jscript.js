e42764d9e84f831e8eb572f73284ae4327595a9e
OlegNeBot <tariana.oleg2012@gmail.com>
2021-04-10 13:09:54 +0300

    finally done

---
+++ jscript.js
@@ -0,0 +1,24 @@
+var key = new Boolean(false)
+var fio;
+
+
+function button() {
+if(key==true){
+Swal.fire('Я это сделал, а ' + fio + ' тебе от меня совет: “ Поступай правильно — поступай в ВятГУ!”')
+}
+else{
+
+{
+Swal.fire('Макет был реализован Головановым Олегом Алексеевичем, вариант 4')
+fio=result.value;
+}
+key=true;
+}
+}
+
+type="text/javascript">$(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm   = $preloader.find('.svg_anm');
+    $svg_anm.fadeOut();
+    $preloader.delay(543).fadeOut('slow');
+});
